095f5e7400990dacca1402315c4b92c11a907be7
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-06-19 00:14:49 +0530

    Range's product

--- sumofeven.js/sum.js
+++ sumofeven.js/sum.js
@@ -1,7 +1,7 @@
 let a = parseInt(process.argv[2]);
 let b = parseInt(process.argv[3]);
 let sum = 0;
-for(let i=a;i<=n;i++){
+for(let i=a;i<=b;i++){
     if(i%2 == 0){
         console.log(sum+i)
     }
